52fd1e43ab95dec14bcdfe3ad3ae90d737cf3f45
Todd Wolfson <todd@twolfson.com>
2013-10-17 01:29:04 -0700

    Continuing refactor. Getting to a more approachable abstract state

---
+++ sublime_harness/logger.py
@@ -0,0 +1,11 @@
+# TODO: Consider using a proper logger
+class Logger(object):
+    def debug(self, msg):
+        pass
+
+    def info(self, msg):
+        pass
+
+    def warn(self, msg):
+        pass
+logger = Logger()
--- sublime_harness/sublime_harness.py
+++ sublime_harness/sublime_harness.py
@@ -2,28 +2,15 @@
 import os
 import shutil
 import subprocess
-import time
-import tempfile
 
 # Load in 3rd party dependencies
 import sublime_info
-from jinja2 import Template
+
+# Load local dependencies
+from .logger import logger
 
 # Set up constants
 __dir__ = os.path.dirname(os.path.abspath(__file__))
-
-# TODO: Consider using a proper logger
-class Logger(object):
-    def debug(self, msg):
-        pass
-
-    def info(self, msg):
-        pass
-
-    def warn(self, msg):
-        pass
-logger = Logger()
-
 
 # TODO: We are going to need a mechanism for utils
 # (e.g. get temporary dir, copy dir, run script in file)
@@ -33,32 +20,23 @@ logger = Logger()
 
 
 class Base(object):
-    plugin_test_dir = os.path.join(sublime_info.get_package_directory(), 'sublime-harness-tmp')
+    plugin_dir = os.path.join(sublime_info.get_package_directory(), 'sublime-harness-tmp')
 
     @classmethod
-    def ensure_plugin_test_dir(cls):
+    def ensure_plugin_dir(cls):
         # If the plugin test directory does not exist, create it
-        if not os.path.exists(cls.plugin_test_dir):
-            os.makedirs(cls.plugin_test_dir)
-
-    @classmethod
-    def _ensure_utils(cls):
-        # Ensure the plugin test directory exists
-        cls.ensure_plugin_test_dir()
-
-        # TODO: Use similar copy model minus the exception
-        # TODO: If we overwrite utils, be sure to wait so that changes for import get picked up
-        if not os.path.exists(cls.plugin_test_dir + '/utils'):
-            shutil.copytree(__dir__ + '/utils', cls.plugin_test_dir + '/utils')
+        if not os.path.exists(cls.plugin_dir):
+            os.makedirs(cls.plugin_dir)
 
     @classmethod
     def install_command_launcher(cls):
         # Guarantee the plugin test dir exists
-        cls.ensure_plugin_test_dir()
+        cls.ensure_plugin_dir()
 
         # If command launcher doesn't exist, copy it
-        orig_command_path = __dir__ + '/launchers/command.py'
-        dest_command_path = cls.plugin_test_dir + '/command_launcher.py'
+        # TODO: Verify this works in Windows
+        orig_command_path = os.path.join(__dir__, '/launchers/command.py')
+        dest_command_path = os.path.join(cls.plugin_dir, '/command_launcher.py')
         if not os.path.exists(dest_command_path):
             shutil.copyfile(orig_command_path, dest_command_path)
         else:
@@ -77,34 +55,34 @@ class Base(object):
                 # and notify the user we must restart Sublime
                 raise Exception('We had to update the test launcher plugin. You must close or restart Sublime to continue testing.')
 
-    _init_launcher_path = plugin_test_dir + '/init_launcher.py'
+    init_launcher_path = os.path.join(plugin_dir, 'init_launcher.py')
 
     @classmethod
     def remove_init_launcher(cls):
         # If the init launcher exists, delete it
-        if os.path.exists(cls._init_launcher_path):
-            os.unlink(cls._init_launcher_path)
+        if os.path.exists(cls.init_launcher_path):
+            os.unlink(cls.init_launcher_path)
 
     @classmethod
     def install_init_launcher(cls):
         # Guarantee the plugin test dir exists
-        cls.ensure_plugin_test_dir()
+        cls.ensure_plugin_dir()
 
         # Clean up any past instances of init launcher
         cls.remove_init_launcher()
 
         # Install a new one
         # TODO: Verify this doesn't have any double invocation consequences
-        orig_command_path = __dir__ + '/launchers/init.py'
-        shutil.copyfile(orig_command_path, cls._init_launcher_path)
+        orig_command_path = os.path.join(__dir__, '/launchers/init.py')
+        shutil.copyfile(orig_command_path, cls.init_launcher_path)
 
     @classmethod
     def run_plugin(cls, plugin_str):
         # Guarantee there is an output directory and launcher
-        cls._ensure_utils()
+        cls.ensure_plugin_dir()
 
         # Output test to directory
-        f = open(cls.plugin_test_dir + '/plugin.py', 'w')
+        f = open(os.path.join(cls.plugin_dir, '/plugin.py'), 'w')
         f.write(plugin_str)
         f.close()
 
@@ -114,7 +92,7 @@ class Base(object):
         if sublime_info.get_sublime_version >= 3000:
             # TODO: Use tasklist for Windows
             # Get process list
-            child = subprocess.Popen(["ps", "ax"], stdout=subprocess.PIPE)
+            child = subprocess.Popen(['ps', 'ax'], stdout=subprocess.PIPE)
             ps_list = str(child.stdout.read())
 
             # Kill the child
