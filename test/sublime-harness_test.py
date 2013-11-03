a1d7b897f26fab12c0ee5498f1ce60b4d0fe44af
Todd Wolfson <todd@twolfson.com>
2013-11-03 14:53:13 -0800

    Adding outlines for test

--- test/sublime-harness_test.py
+++ test/sublime-harness_test.py
@@ -1,6 +1,13 @@
 from unittest import TestCase
 from sublime_harness import sublime_harness
 
+# Outline tests
+"""
+sublime-harness
+    can run arbitrary Python (write to disk)
+    can manipulate Sublime Text (open a view, write content, get content, write fetched content to disk)
+"""
+
 
 class TestRunFunction(TestCase):
     def test_run_exists(self):
