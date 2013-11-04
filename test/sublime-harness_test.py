bec296a7563cdd7595823cf72b3cb576d81e8171
Todd Wolfson <todd@twolfson.com>
2013-11-03 22:29:38 -0800

    Making more notes

--- test/sublime-harness_test.py
+++ test/sublime-harness_test.py
@@ -16,9 +16,10 @@ sublime-harness
             does not execute the code twice
 """
 
-# Decision: Code up v1 as plugin-level only (or maybe allow lower level with no docs/tests)
+# Decision: Code up v1 as plugin-level only
 # and a property that lists out directory
 # Namespaced directory will be created on init
+# DEV: If someone wants low-level access, they have the directory to write to ;)
 
 
 class TestSublimeHarness(unittest.TestCase):
