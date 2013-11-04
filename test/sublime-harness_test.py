59be6a7fe29b0065c85c15b1da242b764a54538f
Todd Wolfson <todd@twolfson.com>
2013-11-03 21:48:46 -0800

    Finishing up first test

--- test/sublime-harness_test.py
+++ test/sublime-harness_test.py
@@ -1,12 +1,15 @@
+import os
+import tempfile
+import time
 from unittest import TestCase
 from sublime_harness import sublime_harness
 
 # Outline tests
 """
 sublime-harness
-    running arbitrary Python (write to disk)
+    running arbitrary Python  # write to disk
         executes the code
-    running Sublime Text specific code (open a view, write content, get content, write fetched content to disk)
+    running Sublime Text specific code  # open a view, write content, get content, write fetched content to disk
         executes within Sublime Text
         when Sublime Text is launched again
             does not execute the code twice
@@ -14,10 +17,20 @@ sublime-harness
 
 
 class TestSublimeHarness(TestCase):
-    def test_run_python(self):
-        """sublime-harness can run arbitrary Python"""
+    def test_running_arbitrary_python(self):
+        # Generate and run our temporary task
+        output_file = tempfile.mkstemp()[1]
         write_to_disk = """
-with open('/tmp/harness', 'w') as f:
-    f.write('hello world')"""
-        with sublime_harness.Harness(write_to_disk):
-            pass
+with open(%s, 'w') as f:
+    f.write('hello world')""" % output_file
+        harness = sublime_harness.Harness(write_to_disk)
+        harness.run()
+
+        # Wait for the file to exist
+        while True:
+            if os.path.exists(output_file):
+                break
+            time.sleep(0.1)
+
+        # Remove the plugin
+        harness.close()
