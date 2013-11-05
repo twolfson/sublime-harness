434eb47c5de7160a1d6a74452fbcfb1c69496778
Todd Wolfson <todd@twolfson.com>
2013-11-04 22:00:36 -0800

    Missed a spot

--- test/test_files/arbitrary.py
+++ test/test_files/arbitrary.py
@@ -1,12 +1,10 @@
 import os
-import time
 import sublime
 
 def run():
-    time.sleep(1)
     f = open('%s', 'w')
     f.write('hello world')
     f.close()
 
-    # if os.environ.get('SUBLIME_AUTO_KILL'):
-    #     sublime.run_command('exit')
+    if os.environ.get('SUBLIME_AUTO_KILL'):
+        sublime.run_command('exit')
