a19b3638d90a975c68fe20dc9b8fdb817f870019
Todd Wolfson <todd@twolfson.com>
2013-12-18 01:43:28 -0800

    Adding HttpLogger

--- sublime_harness/logger.py
+++ sublime_harness/logger.py
@@ -1,11 +1,35 @@
+import os
+try:
+    from urllib.request import urlopen
+    from urllib.error import URLError
+except ImportError:
+    from urllib2 import urlopen, URLError
+
 # TODO: Consider using a proper logger
-class Logger(object):
-    def debug(self, msg):
+class IgnorantLogger(object):
+    def _log(self, level, msg):
         pass
+
+    def debug(self, msg):
+        self._log('debug', msg)
 
     def info(self, msg):
-        pass
+        self._log('info', msg)
 
     def warn(self, msg):
-        pass
-logger = Logger()
+        self._log('warn', msg)
+
+
+# TODO: Robustify this with port and address
+class HttpLogger(IgnorantLogger):
+    def _log(self, level, msg):
+        try:
+            urlopen('http://localhost:7070', data={
+                'level': level,
+                'msg': msg
+            })
+        except URLError:
+            pass
+
+
+logger = HttpLogger() if os.environ.get('HARNESS_LOGGER') else IgnorantLogger()
